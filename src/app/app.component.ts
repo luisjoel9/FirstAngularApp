a9bc26378a5b700e7588d4a87a9926d49483ce75
joel <joel.luis.cr@gmail.com>
2019-03-10 21:59:44 -0400

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  todoArray = [];
+
+  addTodo(value) {
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1)
+      }
+    }
+  }
 }
